9fb447d49f27defbf0a38ba1e10d29871ae5647a
JOEL JACSON.J <166722242+joeljacson@users.noreply.github.com>
2025-03-06 11:24:51 +0530

    mark program is excuted

---
+++ mark.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+public class mark {
+
+    public static void main (String[]args)
+    {
+        Scanner scan =new Scanner(System.in);
+        int mark= scan.nextInt ();
+
+        if (mark>35)
+    {
+        System.out.print("pass");
+    }
+    else{
+        System.out.print("fail");
+    }
+    }
+    
+}
