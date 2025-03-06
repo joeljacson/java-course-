c8a0db093f8eb5a77bd7b87b6c970dd34425bb71
JOEL JACSON.J <166722242+joeljacson@users.noreply.github.com>
2025-03-06 15:46:27 +0530

    cricket program is executed

---
+++ cricket.java
@@ -0,0 +1,19 @@
+public class cricket {
+
+    public static void main (String[]args)
+
+{
+    boolean cricket=false;
+    boolean football=true;
+
+if(cricket||football){
+
+    System.out.print("play ");
+}
+else{
+    System.out.print("dont play");
+}
+
+}
+    
+}
