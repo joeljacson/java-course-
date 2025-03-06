569b9ed13f926deea489c7cdfe2d7f2e7e601d67
JOEL JACSON.J <166722242+joeljacson@users.noreply.github.com>
2025-03-06 10:11:53 +0530

    meghna code is uploaded
    surya weds meghna in parellel universe

---
+++ meghna.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+
+public class meghna {
+    public static void main(String[] args) {
+        Scanner scan =new Scanner (System.in);
+
+        String meghna= scan.nextLine();
+
+        if (meghna.equals("dead")){
+
+            System.out.println("Surya meets ramya");
+        }
+       else{
+        System.out.println("Surya weds meghna");
+
+       }
+    }
+    
+}
