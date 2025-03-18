04991a1a5238a2fd55bd27afd9ac6d16636aee3e
JOEL JACSON.J <166722242+joeljacson@users.noreply.github.com>
2025-03-18 09:20:17 +0530

    this is executed successfully

---
+++ TernaryOperator.java
@@ -0,0 +1,10 @@
+class TernaryOperator{
+
+    public static void main (String[]args)
+    {
+        boolean rain=true;
+
+        String result= (rain)? "take an umbrella":"enjoy the rain";
+        System.out.println(result);
+    }
+}
