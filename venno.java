3f8c0df703582779ef9e56280e90c130c81f9cd3
JOEL JACSON.J <166722242+joeljacson@users.noreply.github.com>
2025-03-19 18:56:05 +0530

    this is executed successfully

---
+++ venno.java
@@ -0,0 +1,20 @@
+
+
+
+class venno{
+    public static void main(String[]args){
+
+    for (int i=1;i<=10;i=i+1){
+
+    
+
+    if(i%2==0)
+    {
+ //System.out.println("even number:"+i);
+
+    }
+else{
+    System.out.println("odd number:"+i);
+} }
+    }
+}
