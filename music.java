d8e2ee83ef0c81e039c804fcfbba9fd9af48561e
JOEL JACSON.J <166722242+joeljacson@users.noreply.github.com>
2025-03-20 18:59:56 +0530

    this is executed successfullt

---
+++ music.java
@@ -0,0 +1,12 @@
+class music{
+
+    public static void main(String[] args) {
+        
+        String[] playlist=new String[4]; 
+
+        playlist[1]= "SONG1";
+        System.out.println(playlist[1]);
+
+        playlist[2]= "SONG2";
+        System.out.println(playlist[2]);
+    }}
